4d11f028b64a66b2b5e1f1b6aff93642b2a3925c
Yan-Tavares <y.cavalcante.ae@gmail.com>
2025-01-10 16:13:37 +0100

    Final version

--- cuisine_plots.py
+++ cuisine_plots.py
@@ -22,14 +22,14 @@ if __name__ == "__main__":
     print(emotion_labels)
 
 
-    #emotion_indexes = [0, 1]
-    #colors = ['red','purple']
+    emotion_indexes = [0, 1]
+    colors = ['red','purple']
     
     # emotion_indexes = [3, 4]  # Replace with your desired emotion indexes
     # colors = ['yellow','gray']
 
-    emotion_indexes = [5, 6]  # Replace with your desired emotion indexes
-    colors = ['blue','green']
+    # emotion_indexes = [5, 6]  # Replace with your desired emotion indexes
+    # colors = ['blue','green']
 
     fig, axes = plt.subplots(1, 2)  # Create a figure with 2 subplots side by side
 
@@ -45,16 +45,49 @@ if __name__ == "__main__":
 
         # Plot the emotion scores as a bar chart with transparency and border
         axes[i].bar(sorted_emotion_scores.keys(), sorted_emotion_scores.values(), alpha=0.7, edgecolor='black', color=colors[i])
-        axes[i].set_xlabel('Cuisine')
-        axes[i].set_ylabel(f'Emotion Score')
-        axes[i].set_title(f'{emotion_name}')
-        axes[i].tick_params(axis='x', rotation=45)  # Tilt the x-axis labels
+        if i == 0:
+            axes[i].set_ylabel(f'Emotion Score',fontsize=18)
+        axes[i].set_title(f'{emotion_name}',fontsize=18)
+        axes[i].tick_params(axis='x', rotation=90,labelsize = 16)  # Tilt the x-axis labels
+
+    
+
 
     plt.subplots_adjust(wspace=2)  # Add more space between the graphs
     plt.tight_layout()
     plt.show()
 
+    # Save the plot
+    fig.savefig(f'results/emotion_scores_{emotion_indexes}.pdf')
 
 
+    #Plot popularity
+    #Collect classified sentences dictionary
 
-    # Get the emotion scores for each sentence
+    with open('results/classified_sentences.json', 'r') as f:
+        classified_sentences = json.load(f)
+
+    # Store the length of the sentences for each cuisine
+    cuisine_lengths = {cuisine: len(classified_sentences[cuisine]['sentences emotions']) for cuisine in classified_sentences}
+    #remove unknown
+    print(cuisine_lengths)
+    cuisine_lengths.pop('Unknown', None)
+
+    # Sort the cuisine lengths by value in descending order
+    sorted_cuisine_lengths = dict(sorted(cuisine_lengths.items(), key=lambda item: item[1], reverse=True))
+
+    
+
+    # Plot the cuisine lengths as a bar chart
+    plt.figure(figsize=(10, 6))
+    plt.bar(sorted_cuisine_lengths.keys(), sorted_cuisine_lengths.values(), alpha=0.7, edgecolor='black', color='black')
+    plt.ylabel('Number of Sentences', fontsize=22)
+    plt.xticks(rotation=90, fontsize=22)
+
+    plt.tight_layout()
+
+    # Save the plot
+    plt.savefig('results/cuisine_popularity.pdf')
+
+    plt.show()
+
